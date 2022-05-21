0dd8ffe88ebcb5ad166ec537a361624b87643d7e
Taha Shaikh <taha_shaikh@sfu.ca>
2022-05-20 23:48:59 -0700

    Solution003 rps

---
+++ js/solution003.js
@@ -0,0 +1,14 @@
+//Rock paper scissors
+
+const rps = (p1, p2) => {
+    if (p1 === p2) {
+        return 'Draw!'
+    }
+
+    if (p1 === 'scissors' && p2 === 'paper' || p1 === 'rock' && p2 === 'scissors' || p1 === 'paper' && p2 === 'rock') {
+        return 'Player 1 won!'
+    }
+    else if (p1 === 'scissors' && p2 === 'rock' || p1 === 'paper' && p2 === 'scissors' || p1 === 'rock' && p2 === 'paper') {
+        return 'Player 2 won!'
+    }
+};
